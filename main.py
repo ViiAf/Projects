805725a9179815d90e4123daca0db9b169d85efe
ViiAf <118045299+ViiAf@users.noreply.github.com>
2023-11-25 12:05:25 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# Write your solution below
+# Follow the instructions in the tab to the right
+
+# Use this exchange rate
+NAIRA_PER_DOLLAR = 410.59 # exchange rate as of Nov 10 2021
+USD_input = float(input("enter the amount of USD to convert to NGN: "))
+Naira = USD_input * NAIRA_PER_DOLLAR
+# printing out the results after converting dollar to naira
+print(f"{USD_input: .2f} USD is" f"{Naira: .2f} NGN")
+
+# print(f"{USD_input:.3f}" + " USD is " + f"{Naira: .2f}" + "NGN")
