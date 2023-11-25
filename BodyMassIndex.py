9806d2873e039dc570e57438c08068cd2e59bede
ViiAf <118045299+ViiAf@users.noreply.github.com>
2023-11-25 12:16:53 +0000

    Update BodyMassIndex.py

--- BodyMassIndex.py
+++ BodyMassIndex.py
@@ -1,13 +1,12 @@
-# TODO 1: Replace the below code to accept the "weight" and "height" values as input from the user
+# Code to accept the "weight" and "height" values as input from the user
 weight = float(input("Enter your weight in kilograms: "))
 height = float(input("Enter your height in meters: "))
 
-# TODO 2: Calculate the BMI value based on the weight and height values
+#  Calculate the BMI value based on the weight and height values
 
 BMI = weight / height**2
 
-# This is a handy function to round the number to a 1 decimal point
-# Don't change the code below
+# Rounds the value
 BMI = round(BMI, 1)
 
 print(f"The body mass index is: {BMI}")
